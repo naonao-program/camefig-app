eea604611cd8f0933c0fe793ce61aab293a042aa
naonao-program <gf19a012@oecu.jp>
2021-09-19 19:17:39 +0900

    topのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: 'homes#top'
-=======
-  root to: 'main#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> 3fd9cb32072884a5d033ef429d052eb0135304f9
 end
