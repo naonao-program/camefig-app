6703b14780a2b625ef0f50a52f3a42c4d279c1ac
naonao-program <gf19a012@oecu.jp>
2021-09-22 17:49:13 +0900

    createアクションをコントローラーに追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,18 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+  end
+
   def show
     @photo = Photo.find(params[:id])
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:genre,:f_number,:shutter_speed,:iso,:month,:time,:camera_name,:lens_name,:accessories,:image).merge(user_id: current_user.id)
   end
 end
