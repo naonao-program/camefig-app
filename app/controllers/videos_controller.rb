fa6c61580f0b1259e17cfa25e8d5334d34561d96
naonao-program <gf19a012@oecu.jp>
2021-10-14 16:06:05 +0900

    video_controller showアクションを追加

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,20 @@
 class VideosController < ApplicationController
   def create
     @video = Video.new(video_params)
+    if @video.save
+      redirect_to video_path(@video), notice: "投稿が完了しました"
+    else
+      @photo = Photo.new
+      render 'homes/upload'
+    end
+  end
+
+  def show
+    @video = Video.find(params[:id])
   end
   
   private
   def video_params
-    params.require(:video).permit(:youtube_url, :month, :fps, :tips, :camera_name, :lens_name, :accessories).merge(user_id: current_user.id, month:[:video][:month].to_i)
+    params.require(:video).permit(:youtube_url, :month, :fps, :tips, :camera_name, :lens_name, :accessories).merge(user_id: current_user.id, month: params[:video][:month].to_i)
   end
 end
-
