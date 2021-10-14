558bc5b402964b15618e72238d91f2ff2742a4f8
naonao-program <gf19a012@oecu.jp>
2021-10-14 15:48:33 +0900

    no method errorを解決

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,12 +1,11 @@
 class VideosController < ApplicationController
   def create
-    def create
-      @video = Video.new(video_params)
-    end
+    @video = Video.new(video_params)
+  end
   
-    private
-    def video_params
-      params.require(:video).permit(:youtube_url, :month, :fps, :tips, :camera_name, :lens_name, :accessories).merge(user_id: current_user.id, month:[:video][:month].to_i)
-    end
+  private
+  def video_params
+    params.require(:video).permit(:youtube_url, :month, :fps, :tips, :camera_name, :lens_name, :accessories).merge(user_id: current_user.id, month:[:video][:month].to_i)
   end
 end
+
