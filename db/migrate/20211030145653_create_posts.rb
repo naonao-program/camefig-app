bdd7698f984265e98c3cc0d075ce676fdef27c5f
naonao-program <gf19a012@oecu.jp>
2021-10-31 00:00:24 +0900

    モデルを作成しrails db:migrateを実行

---
+++ db/migrate/20211030145653_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :video
+      t.string :youtube_url
+      t.string :fps
+      t.text :tips
+      t.string :camera_name
+      t.string :lens_name
+      t.string :accessories
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_084525) do
+ActiveRecord::Schema.define(version: 2021_10_30_145653) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 2021_09_20_084525) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "video"
+    t.string "youtube_url"
+    t.string "fps"
+    t.text "tips"
+    t.string "camera_name"
+    t.string "lens_name"
+    t.string "accessories"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
