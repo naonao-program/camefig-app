dfd82632b4d909960f7210a8f725760d8b05c339
naonao-program <gf19a012@oecu.jp>
2021-11-16 00:11:13 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_11_07_095001) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -81,5 +80,4 @@ ActiveRecord::Schema.define(version: 2021_11_07_095001) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "photos", "users"
   add_foreign_key "posts", "users"
-
 end
