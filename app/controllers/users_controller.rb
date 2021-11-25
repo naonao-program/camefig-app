941913e7178667b2e9c28413044c2ec38ecdec04
naonao-program <gf19a012@oecu.jp>
2021-11-25 14:44:40 +0900

    tabを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = @user.photos.order(id: "DESC")
+    @videos = @user.posts.order(id: "DESC")
   end
 end
